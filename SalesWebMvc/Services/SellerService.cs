640a21d568fbc31ee7cea6c04fef66902a8d2c1c
Joao Melo <devjoaomelo@gmail.com>
2024-06-17 19:56:54 -0300

    Update seller and custom exceptions

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,8 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
+
 namespace SalesWebMvc.Services
 {
     public class SellerService
@@ -29,10 +31,28 @@ namespace SalesWebMvc.Services
         }
 
         public void Remove(int id)
-        { 
+        {
             var obj = this.FindById(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
+            
         }
     }
 }
