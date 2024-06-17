188d60f5e11ef655e0cd8c8279e4a9e40bf64833
Joao Melo <devjoaomelo@gmail.com>
2024-06-17 11:27:57 -0300

    Other entities and migrations

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,42 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BrithDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); // Associação 1 pra vários SalesRecord
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime brithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BrithDate = brithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            Sales.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            Sales.Remove(sales);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sales => sales.Date >= initial && sales.Date <= final)
+                        .Sum(sales => sales.Amount);
+        }
+
+    }
+}
