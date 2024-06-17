add03761889443e13536a0b6154d9cf6cb9fe5b3
Joao Melo <devjoaomelo@gmail.com>
2024-06-17 18:53:37 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,5 +22,17 @@ namespace SalesWebMvc.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+        }
+
+        public void Remove(int id)
+        { 
+            var obj = this.FindById(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
